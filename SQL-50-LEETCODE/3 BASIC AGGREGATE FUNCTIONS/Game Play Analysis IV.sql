01803e9a984eaf804a33904335559f31e9eb1dfe
Sudipta Sundar Sahoo <sudipta.sundar95@gmail.com>
2025-07-07 23:26:19 +0530

    updates

--- SQL-50-LEETCODE/3 BASIC AGGREGATE FUNCTIONS/Game Play Analysis IV.sql
+++ SQL-50-LEETCODE/3 BASIC AGGREGATE FUNCTIONS/Game Play Analysis IV.sql
@@ -1,13 +1,9 @@
 # Write your MySQL query statement below
 select round(count(b.player_id)/count(first_logins.player_id), 2) as fraction
 from (
-    select player_id, event_date
-    from activity a
-    where event_date = (
-        select min(event_date)
-        from activity
-        where player_id = a.player_id
-    )
+    select player_id, min(event_date) first_date
+    from activity
+    group by player_id
 ) as first_logins
 left join activity b
-on b.player_id = first_logins.player_id and datediff(b.event_date,first_logins.event_date) = 1;
+on b.player_id = first_logins.player_id and datediff(b.event_date,first_logins.first_date) = 1;
--- SQL-50-LEETCODE/3 BASIC AGGREGATE FUNCTIONS/Percentage of Users Attended a Contest.sql
+++ SQL-50-LEETCODE/3 BASIC AGGREGATE FUNCTIONS/Percentage of Users Attended a Contest.sql
@@ -1,7 +1,6 @@
--- Write your PostgreSQL query statement below
-select r.contest_id, round(count(distinct r.user_id)*100.0/total.total_users, 2) percentage
+select r.contest_id, round(count(r.user_id)*100.0/total.total_users, 2) percentage
 from register r
 join (select count(*) as total_users from users) total
 on true
-group by r.contest_id, total.total_users
+group by r.contest_id
 order by percentage desc, r.contest_id asc;
